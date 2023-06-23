5e6d9a6f4644ed9949d41d027923024f6f7e63dc
Devdpprakash <devprakashdp2021@gmail.com>
2023-06-23 23:29:36 +0530

    first commit

---
+++ server/config/dbConfig.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+require("dotenv").config();
+
+const connectionParams={
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}
+mongoose.connect(process.env.mongo_uri, connectionParams);
+const connection = mongoose.connection;
+
+connection.on("connected", ()=>{
+    console.log("MongoDB connected successfully");
+})
+
+connection.on('error', (err)=>{
+    console.log("MongoDB connection failed");
+})
