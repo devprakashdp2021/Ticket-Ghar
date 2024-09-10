46791d0f2384150dced9e0ae7392ff2e3f6d1149
Dev Prakash <97429564+devprakashdp2021@users.noreply.github.com>
2024-09-11 01:40:42 +0530

    Updated client folder path in server.js for render deploymnet

--- server/server.js
+++ server/server.js
@@ -20,9 +20,9 @@ __dirname = path.resolve();
 
 //render deployment
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/client/build")));
+    app.use(express.static(path.join(__dirname, "../client/build")));
     app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
 }
 
